22414566530938d9c8727608ca66578af484f18a
Jayesh Gupta <jayeshgpr2856@gmail.com>
2022-08-02 23:25:22 +0530

    to do list

---
+++ app.js
@@ -0,0 +1,28 @@
+//jshint esversion: 6
+
+const express = require("express");
+const bodyParser = require("body-parser");
+
+const app = express();
+
+app.set("veiw engine", "ejs");
+app.get("/", function(req, res){
+    
+    var today = new Date();
+    var currentDay = today.getDay();
+    var day = "";
+
+    if(currentDay === 6 || currentDay === 0){
+        day = "Weekend"
+    } 
+    else {
+        res.sendFile(__dirname + "/index.html");
+        day = "Weekday";
+    }
+
+    res.render("list", {kindOfDay: day});
+});
+
+app.listen(3000, function() {
+    console.log("Server started on port 3000");
+});
